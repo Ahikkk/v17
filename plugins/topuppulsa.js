70b9d36bacfc21e285dd809ba338f053b2f08d1b
Ahikkk <98951049+Ahikkk@users.noreply.github.com>
2022-04-03 08:28:03 +0700

    Update topuppulsa.js

--- plugins/topuppulsa.js
+++ plugins/topuppulsa.js
@@ -1,17 +1,7 @@
 const topuppulsa = (prefix) => { 
-	return `*HALLO KAKðŸ‘‹*
-*PILIH DIBAWAH YA!!!*
-• 5k
-• 10k
-• 20k
-• 30k
-• 40k
-• 50k
-• 60k
-• 70k
-• 80k
-• 90k
-• 100k
+	return `*HALLO KAKÃ°Å¸â€˜â€¹*
+UNTUK HARGA ROLE INI ADALAH 
+800 WLS RGT
 *minat? hub :* http://wa.me/${nomorowner}
 `
 }
