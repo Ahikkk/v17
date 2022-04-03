3fb95127f41f2115741d0f1eb11603beef2995ef
Ahikkk <98951049+Ahikkk@users.noreply.github.com>
2022-04-03 08:30:14 +0700

    Update topupfreefire.js

--- plugins/topupfreefire.js
+++ plugins/topupfreefire.js
@@ -1,23 +1,6 @@
 const topupfreefire = (prefix) => { 
 	return `*HALLO KAK👋*
-*PILIH DIBAWAH YA!!!*
-*LIST DM FF*
-*NO ILEGAL 100%*
-5.500 = 20💎
-9.500 = 50💎
-11.500 = 70💎
-17.500 = 100💎
-21.500 = 140💎
-32.500 = 210💎
-52.500 = 355💎
-77.500 = 510💎
-105.300 = 720💎
-205.300 = 1450💎
-311.200 = 2180💎
-510.600 = 3640💎
-1.050.100 = 17280💎
-*MM* : 29.300
-*MB*  : 141.600
+UNTUK HARGA BGL DISINI ADALAH 5 BGL/1 WLS RGT 
 *minat? hub :* http://wa.me/${nomorowner}
 `
 }
