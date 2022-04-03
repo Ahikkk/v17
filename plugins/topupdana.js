a88d929af5d2825980a68129d8c33ff78873f630
Ahikkk <98951049+Ahikkk@users.noreply.github.com>
2022-04-03 08:26:08 +0700

    Update topupdana.js

--- plugins/topupdana.js
+++ plugins/topupdana.js
@@ -1,17 +1,8 @@
 const topupdana = (prefix) => { 
-	return `*HALLO KAKðŸ‘‹*
+	return `*HALLO KAKÃ°Å¸â€˜â€¹*
 *PILIH DIBAWAH YA!!!*
-• 5k
-• 10k
-• 20k
-• 30k
-• 40k
-• 50k
-• 60k
-• 70k
-• 80k
-• 90k
-• 100k
+UNTUK HARGA ROLE INI ADALAH 
+50 WLS RGT
 *minat? hub :* http://wa.me/${nomorowner}
 `
 }
